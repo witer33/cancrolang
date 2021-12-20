7dd568ee3b96b51195bc94f36407b38d5a3fa1eb
witer33 <leox337@gmail.com>
2021-12-20 14:36:08 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import argparse
+import interpreter
 
 parser = argparse.ArgumentParser(
     description='Encode a brainfuck file in the cancro language and encode a cancro file in the brainfuck language.'
@@ -7,6 +8,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--file", "-f", help="The file to encode")
 parser.add_argument("--output", "-o", help="The output file", default="out")
 parser.add_argument("--decode", "-d", help="Decode a cancro file", action="store_true")
+parser.add_argument("--run", "-r", help="Run the file", action="store_true")
 
 chars_map = {
     '>': 'Cancro',
@@ -22,15 +24,27 @@ chars_map = {
 encode_map = str.maketrans(chars_map)
 decode_map = {v: k for k, v in chars_map.items()}
 
+
+def encode(data: str) -> str:
+    return data.translate(encode_map)
+
+
+def decode(data: str) -> str:
+    for key, value in decode_map.items():
+        data = data.replace(key, value)
+    return data
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    with open(args.file, 'r') as f:
-        content = f.read()
-    if args.decode:
-        for key, value in decode_map.items():
-            content = content.replace(key, value)
+    if args.run:
+        interpreter.BrainfuckInterpreter(interpreter.CancroReader(open(args.file, "rb"))).run()
     else:
-        content = content.translate(decode_map)
-    with open(args.output, 'w') as f:
-        f.write(content)
-
+        with open(args.file, 'r') as f:
+            content = f.read()
+        if args.decode:
+            content = decode(content)
+        else:
+            content = encode(content)
+        with open(args.output, 'w') as f:
+            f.write(content)
